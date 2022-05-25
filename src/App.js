c76493786b1f60e917152bf09d71f54a625966e1
Ahmed Eid <ahmedeid.webdev@gmail.com>
2022-05-25 17:37:03 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>hello mcq</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
